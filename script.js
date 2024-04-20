2cf9ea572f2ac2c59191b2366fcbc4a65367a0b1
justin <64299580+aceus0@users.noreply.github.com>
2024-04-20 16:44:06 -0400

    made gui, and added interactivity for num buttons

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log(`Test`);
+let num1 = 0;
+let num2 = 0;
+let operator = 0;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case `plus`:
+        return add(a, b);
+        case `minus`:
+        return subtract(a, b);
+        case `times`:
+        return multiply(a, b);
+        case `divide`:
+        return divide(a, b);
+    }
+}
+
+console.log(operate(10, 5, ));
+
+
+
+//Listeners
+
+const numButtons = document.querySelector(`.numbers`);
+const row = document.querySelectorAll(`.row`)
+
+numButtons.addEventListener(`click`, (e)=> {
+    if (e.target.tagName === `BUTTON`){
+        input = e.target.textContent;
+        console.log(input);
+    }
+
+})
