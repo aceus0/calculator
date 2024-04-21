279523ef356c9b6a5cb821526ce0931726352857
justin <64299580+aceus0@users.noreply.github.com>
2024-04-20 20:52:43 -0400

    calculator is completely functional. can handle floating point numbers and negatives.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//Varible initilization
 let num1 = 0;
 let num2 = 0;
 let operator = ``;
 let result = ``;
 let hasRun = false;
+const operators = [`÷`,`x`,`-`,`+`]
+let input = ``;
+let manyDecimal = false;
 
+//Functions
 function add(a, b) {
     return a + b;
 }
@@ -17,6 +22,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+    if (b === 0) {
+        return `no`;
+    } 
     return a / b;
 }
 
@@ -37,33 +45,43 @@ function clear(){
     num1 = 0
     num2 = 0
     result = 0
+    operator = ``;
     input = ``
     display.textContent = ``
 }
 
+function hasDecimal(string){
+    let count = (string.match(/\./g) || []).length;
+    return count !== 0;
+}
 
-
-//Listeners
+//DOM Manipulation Setup and Listeners
 const operatorButtons = document.querySelector(`.operators`);
 const numButtons = document.querySelector(`.numbers`);
 const equal = document.querySelector(`#equal`);
 const clearDisplay = document.querySelector(`#clear`);
-const display = document.querySelector(`.displayBox`)
-let input = ``;
+const display = document.querySelector(`.displayBox`);
+const negative = document.querySelector(`#negative`)
+
 display.textContent = ``;
 
+
+
 numButtons.addEventListener(`click`, (e)=> {
-    if (e.target.tagName === `BUTTON`){
+    if (e.target.tagName === `BUTTON` && ![`negative`].includes(e.target.id)){
         if (hasRun === true){
             hasRun = false;
             clear()
-            console.log(e.target.textContent);
             input = input + e.target.textContent;
+            manyDecimal = hasDecimal(input);
             display.textContent = input;
             return input;
+        } else if (e.target.textContent === '.' && manyDecimal === true) {
+            return
         } else {
         console.log(e.target.textContent);
         input = input + e.target.textContent;
+        manyDecimal = hasDecimal(input);
         display.textContent = input;
         return input;
         } 
@@ -72,34 +90,70 @@ numButtons.addEventListener(`click`, (e)=> {
     }
 )
 
+
+
 operatorButtons.addEventListener(`click`, (e)=> {
     if (e.target.tagName === `BUTTON` && ![`equal`, `clear`].includes(e.target.id)){
-        if (result != ``){
-        operator = e.target.textContent;
-        hasRun = false;
-        return operator;
-        }
-        display.textContent = ``;
-        num1 = parseInt(input);
-        console.log(num1);
-        operator = e.target.textContent;
-        input = ``;
-        return [num1, operator];
-        
-        }
+        if (input === '' && hasRun === false){
+            return
+        }else if (result != ``){
+            operator = e.target.textContent;
+            hasRun = false;
+            return operator;
+
+            } else if (operators.includes(operator)){
+                tempNum = parseFloat(input);
+                num1 = operate(num1, tempNum, operator);
+                display.textContent = num1
+                input = ``;
+                operator = e.target.textContent;
+                return num1; 
+            }
+            
+            num1 = parseFloat(input);
+            console.log(num1);
+            operator = e.target.textContent;
+            input = ``;
+            return [num1, operator];
+            
+            }
     }
 )
 
 equal.addEventListener(`click`, (e) =>{
-    num2 = parseInt(input);
+    if (operator === ''){
+        return
+    }
+    num2 = parseFloat(input);
     console.log(num2);
     result = operate(num1, num2, operator);
-    display.textContent = result;
-    num1 = result;
-    input = ``;
-    hasRun = true;
+    if (result === `no`) {
+        display.textContent = `DO NOT DO THAT!`;
+        result = ``;
+        input = ``;
+        hasRun = true;
+    } else {
+        result = Math.round((result + Number.EPSILON) * 100) / 100
+        display.textContent = result;
+        num1 = result;
+        input = ``;
+        hasRun = true;
+    }
 })
+
 
 clearDisplay.addEventListener(`click`, (e) =>{
     clear();
+})
+
+negative.addEventListener(`click`, (e)=> {
+    let neg = `-`;
+    if(input.includes(neg)){
+        input = input.substring(1);
+        display.textContent = ``;
+        display.textContent = input;
+    } else {
+    input = `-${input}`;
+    display.textContent = input;
+    }
 })
